b5a29717773ea664bd8fd504863d4d76c3bac47d
Suttie23 <76448183+Suttie23@users.noreply.github.com>
2022-12-06 14:08:28 +0000

    First attempt and Non-Recursive Serial

--- nqueens.cpp
+++ nqueens.cpp
@@ -1,4 +1,4 @@
- // Headers
+// Headers
 #include <cassert>
 #include <iostream>
 #include <vector>
@@ -7,7 +7,7 @@
 #include <chrono>
 #include <iomanip>
 
-using namespace std; 
+using namespace std;
 
 /*
 *   The below N-Queens chessboard formulation is as follows:
@@ -22,16 +22,16 @@ using namespace std;
 *           After each such placement in the second row, test the state of the chessboard. If it's still valid, then
 *               Try placing a queen at each column in the THIRD row (the first and second rows already store a queen placement at columns there)
 *               ...
-* 
+*
 *    This algorithm is recursive: It applies the same logic again and again, while modifying the internal state.
 *    GPUs and parallelism DO NOT WORK WELL WITH RECURSION. So, you need to come up with a solution that achieves the same results WITHOUT RECURSION, so that you can then convert it to OpenMP and GPU
-*    Feel free to use existing resources (e.g. how to remove recursion), but REFERENCE EVERYTHING YOU USE, but DON'T COPY-PASTE ANY SOLUTION FROM ANY OBSCURE WEBSITES. 
+*    Feel free to use existing resources (e.g. how to remove recursion), but REFERENCE EVERYTHING YOU USE, but DON'T COPY-PASTE ANY SOLUTION FROM ANY OBSCURE WEBSITES.
 */
 
 // check if the chessboard is valid so far, for row in [0,lastPlacedRow]
 bool boardIsValidSoFar(int lastPlacedRow, const std::vector<int>& gameBoard)
 {
-    const auto N = gameBoard.size(); 
+    const auto N = gameBoard.size();
     int lastPlacedColumn = gameBoard[lastPlacedRow];
 
     // Check against other queens
@@ -75,11 +75,11 @@ void calculateAllSolutions(int N, bool print)
     std::vector<int> gameBoard(N, 0);
     calculateSolutionsRecursive(0, gameBoard, N, solutions);
     printf("N=%d, solutions=%d\n", N, int(solutions.size()));
-    
+
     if (print)
     {
         std::string text;
-        text.resize(N * (N + 1)+1); // we know exactly how many characters we'll need: one for each place at the board, and N newlines (at the end of each row). And one more newline to differentiate from other solutions
+        text.resize(N * (N + 1) + 1); // we know exactly how many characters we'll need: one for each place at the board, and N newlines (at the end of each row). And one more newline to differentiate from other solutions
         text.back() = '\n'; // add extra line at the end
         for (const auto& solution : solutions)
         {
@@ -87,7 +87,7 @@ void calculateAllSolutions(int N, bool print)
             {
                 auto queenAtRow = solution[i];
                 for (int j = 0; j < N; ++j)
-                    text[i * (N+1) + j] = queenAtRow == j ? 'X' : '.';
+                    text[i * (N + 1) + j] = queenAtRow == j ? 'X' : '.';
                 text[i * (N + 1) + N] = '\n';
             }
             std::cout << text << "\n";
