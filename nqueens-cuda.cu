6294a31b8b8cf859efdf6117884774857341cc25
Suttie <76448183+Suttie23@users.noreply.github.com>
2022-12-09 15:22:51 +0000

    Copied over Non-Recursive Solution to CUDA
    Copied over Non-Recursive Solution to act as a starting point for solving the CUDA problem.

--- nqueens-cuda.cu
+++ nqueens-cuda.cu
@@ -1,4 +1,12 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <chrono>
+#include <iomanip>
+#include <stack>
+#include <thread>
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 
@@ -7,26 +15,149 @@
 using namespace std;
 
 
-int main(int argc, char **argv)
+#define N_MAX 10
+
+// Determine the validity of the board
+bool boardIsValid(int lastPlacedRow, const int* gameBoard, const int N)
 {
-    // Get number of devices on system
-    int deviceCount; 
-    gpuErrchk(cudaGetDeviceCount(&deviceCount));
+    // Last placed column
+    int lastPlacedColumn = gameBoard[lastPlacedRow];
+    volatile bool valid = true;
 
-    cout << "Number of devices: " << deviceCount << endl;
-    for (int i = 0; i < deviceCount; ++i) 
+
+    // Iterating to determine whether the board is valid
+    for (int row = 0; row < lastPlacedRow; ++row)
     {
-        // Get properties for device
-        cudaDeviceProp deviceProp;
-        cudaGetDeviceProperties(&deviceProp, i);
 
-        cout << "Device " << i << endl;
-        cout << "Name " << deviceProp.name << endl;
-        cout << "Revision " << deviceProp.major << "." << deviceProp.minor << endl;
-        cout << "Memory " << deviceProp.totalGlobalMem / 1024 / 1024 << "MB" << endl;
-        cout << "Warp Size " << deviceProp.warpSize << endl;
-        cout << "Clock " << deviceProp.clockRate << endl;
-        cout << "Multiprocessors " << deviceProp.multiProcessorCount << endl;
-    } 
+        // If the row is the same as the last column, then the board is not valid
+        if (gameBoard[row] == lastPlacedColumn)
+            return false;
+
+        // check the 2 diagonals
+        const auto col1 = lastPlacedColumn - (lastPlacedRow - row);
+        const auto col2 = lastPlacedColumn + (lastPlacedRow - row);
+
+        // If the row is the came as column 1 or 2, the board is not valid
+        if (gameBoard[row] == col1 || gameBoard[row] == col2)
+            return false;
+    }
+    // The board is valid if nothing is flagged previously
+    return true;
+}
+
+// Calculate the solutions
+void calculateSolutions(int N, std::vector<std::vector<int>>& solutions)
+{
+    // For board evaluation
+    const long long int O = powl(N, N);
+
+    // Solution array & number of solutions
+    int* solutionArr = (int*)malloc(pow(N, 5) * sizeof(int)); // Determining array size 
+    int no_of_sols = 0;
+
+    // Checking all possible Queen Positions
+    for (long long int i = 0; i < O; i++) {
+        bool valid = true;
+
+        // Game Board array
+        int gameBoard[N_MAX];
+        long long int column = i;
+
+        for (int j = 0; j < N; j++) {
+            gameBoard[j] = column % N;
+
+            // If the board is not valid, break
+            if (!boardIsValid(j, gameBoard, N)) {
+                valid = false;
+                break;
+            }
+
+            // divide and assign to column
+            column /= N;
+        }
+
+        // If the board is valid, 
+        if (valid) {
+            for (int j = 0; j < N; j++)
+                solutionArr[N * no_of_sols + j] = gameBoard[j];
+            // Increment number of solutions found
+            no_of_sols++;
+        }
+    }
+
+    // Add the solution to the solutions array
+    for (int i = 0; i < no_of_sols; i++) {
+        std::vector<int> solution = std::vector<int>();
+        for (int j = 0; j < N; j++)
+            solution.push_back(solutionArr[N * i + j]);
+        solutions.push_back(solution);
+    }
+    // Free memory
+    free(solutionArr);
+}
+
+// Calculate all solutions given the size of the chessboard
+void calculateAllSolutions(int N, bool print)
+{
+    std::vector<std::vector<int>> solutions;
+
+    // Start timer
+    //auto startTime = omp_get_wtime();
+
+    // Calculate solutions
+    //calculateSolutions(N, solutions);
+
+    // End timer
+    //auto endTime = omp_get_wtime();
+
+    // Calculate time
+   // auto overallTime = endTime - startTime;
+
+    // Print to console
+    std::cout << "N=" << N << " Solution time taken: " << "overalltime" << "s\n";
+    printf("N=%d, solutions=%d\n\n", N, int(solutions.size()));
+
+}
+
+
+int main(int argc, char** argv)
+{
+
+    for (int N = 4; N <= N_MAX; ++N)
+        calculateAllSolutions(N, false);
+
+    /*
+    // Input Specific N for solution
+{
+    int n;
+    char c;
+
+    do
+    {
+        cout << "-= NQueens Puzzle Solutions\n";
+        cout << "-= WARNING: N > 9 will take longer to process \n";
+        cout << "-= N = 10 takes around 75 seconds on my PC \n\n";
+
+        do
+        {
+            cout << "\tEnter an between 3 and 15 (not-inclusive) \n";
+            cout << "\nN = ";
+            cin >> n;
+            if (n < 4 || n > 14)
+                cout << "INVALID!\n";
+        } while (n < 4 || n > 14);
+
+        calculateAllSolutions(n, false);
+
+        cout << "Solve another N-Queen Puzzle (Y/N) ? ";
+        cin >> c;
+
+        cout << "\n\n";
+    } while (c != 'N' && c != 'n');
+
+    system("PAUSE");
     return 0;
+}
+*/
+
 }
